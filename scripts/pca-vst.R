1e7f1aed834c9562d834499a5df9f9a4b324670b
Svitlana Myroshnychenko <svimyr@student.chalmers.se>
2026-02-05 10:45:57 +0100

    alignmen-hisat2 script  renamed, deleted

--- scripts/pca-vst.R
+++
@@ -1,9 +0,0 @@
-library(DESeq2)
-
-
-
-# Upload count matrix
-counts <- read.table("results/counts/gene_counts.txt", header=TRUE, row.names=1, check.names=FALSE)
-
-# Сheck first 5 rows
-head(counts)
